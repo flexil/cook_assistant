0652e7dbb7a531bea5ea32dcbce6a04a6cd8ce5d
Maximilien Kpizingui <kpiz1@hotmail.com>
2023-06-05 23:32:47 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""This code is  written by Maximilien contact me via mail if you have any questions e.maximilien.k@protonmail.com"""
+"""Python file to serve as the frontend"""
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -9,10 +9,8 @@ from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 token = os.environ.get("openai-key")
-
 llm = OpenAI(temperature=1, openai_api_key=token)
 def load_chain_loc():
 	template = """Your job is to come up with a classic dish from the area that the users suggests.
@@ -27,6 +25,9 @@ def load_chain_loc():
 	location_chain = LLMChain(llm=llm, prompt=prompt_template)
 	return location_chain
 
+loc_chain = load_chain_loc()
+
+
 def  load_chain_meal():
 	template = """Given a meal, give a short and simple recipe on how to make that dish at home.
 	% MEAL
@@ -39,15 +40,23 @@ def  load_chain_meal():
 	# Holds my 'meal' chain
 	meal_chain = LLMChain(llm=llm, prompt=prompt_template)
 	return meal_chain
-
-loc_chain = load_chain_loc()	
+	
 chain_meal =load_chain_meal()
 
 overall_chain = SimpleSequentialChain(chains=[loc_chain,chain_meal], verbose=True)
 
-st.set_page_config(page_title=" Cook bot", page_icon=":robot:")
-st.title("Cook bot powered with LLMs")
-st.write("By Maximilien ")
+	
+# From here down is all the StreamLit UI.
+st.set_page_config(page_title=" Cook Assistant ", page_icon=":robot:")
+st.title("Cook Assitant  Powered By  LLMs")
+st.subheader('AI Web App implemented by [Maximilien Kpizingui](https://kpizmax.hashnode.dev)')
+'''
+The cooking assistant recommends personalized recipes based on your favorite city, provide expert cooking tips, and take your culinary skills to new heights! 
+'''
+st.image("@maximilien.png")
+
+st.sidebar.markdown("Email: maximilien@tutanota.de")
+st.sidebar.markdown("Element: @maximilien:matrix.org")
 
 if "generated" not in st.session_state:
 	st.session_state["generated"] = []
@@ -57,8 +66,8 @@ if "past" not in st.session_state:
 
 
 def get_text():
-	st.header("enter the city you want to know the best food")
-	input_text = st.text_input("", key="input")
+	st.subheader("Please enter the city to know the best recipe")
+	input_text = st.text_input("waiting for input", key="input")
 	return input_text
 
 
@@ -69,7 +78,7 @@ if user_input:
 
 	st.session_state.past.append(user_input)
 	st.session_state.generated.append(output)
-
+#	st.write(output)
 if st.session_state["generated"]:
 
 	for i in range(len(st.session_state["generated"]) - 1, -1, -1):
